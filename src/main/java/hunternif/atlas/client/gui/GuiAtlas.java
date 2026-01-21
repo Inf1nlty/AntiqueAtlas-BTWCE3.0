81d0dc5e80026a4653f03988e5b29fae50b4c6ec
Inf1nity <1449896523@qq.com>
2026-01-21 18:10:24 +0800

    Initial commit

--- src/main/java/hunternif/atlas/client/gui/GuiAtlas.java
+++ src/main/java/hunternif/atlas/client/gui/GuiAtlas.java
@@ -1,8 +1,3 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package hunternif.atlas.client.gui;
 
 import hunternif.atlas.AntiqueAtlasItems;
