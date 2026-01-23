5054c851a93f3b023900743bb1a61f6e68113ee4
Inf1nity <1449896523@qq.com>
2026-01-23 16:14:16 +0800

    Fix

--- src/main/java/hunternif/atlas/network/AtlasVanillaPacketRegistry.java
+++ src/main/java/hunternif/atlas/network/AtlasVanillaPacketRegistry.java
@@ -2,18 +2,18 @@ package hunternif.atlas.network;
 
 import net.minecraft.src.Packet;
 
-import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 public final class AtlasVanillaPacketRegistry {
-    private static boolean done = false;
+
+    private static final Map<Integer, Class<? extends Packet>> idToClassMap = new HashMap<>();
+    private static final Map<Class<? extends Packet>, Integer> classToIdMap = new HashMap<>();
 
     private AtlasVanillaPacketRegistry() {
     }
 
     public static synchronized void registerAll() {
-        if (done) return;
-        done = true;
-
         register(230, MapDataPacket.class);
         register(231, TilesPacket.class);
         register(232, MarkersPacket.class);
@@ -24,13 +24,23 @@ public final class AtlasVanillaPacketRegistry {
         register(237, RegisterTileIdPacket.class);
     }
 
-    private static void register(int id, Class<? extends Packet> cls) {
-        try {
-            Method m = Packet.class.getDeclaredMethod("addIdClassMapping", int.class, boolean.class, boolean.class, Class.class);
-            m.setAccessible(true);
-            m.invoke(null, id, true, true, cls);
-        } catch (Throwable t) {
-            throw new RuntimeException("Failed to register packet " + cls.getName() + " with id " + id, t);
+    private static void register(int id, Class<? extends Packet> packetClass) {
+        if (idToClassMap.containsKey(id)) {
+            throw new IllegalArgumentException("Packet ID " + id + " is already registered!");
         }
+        if (classToIdMap.containsKey(packetClass)) {
+            throw new IllegalArgumentException("Packet class " + packetClass.getName() + " is already registered!");
+        }
+        idToClassMap.put(id, packetClass);
+        classToIdMap.put(packetClass, id);
+        Packet.addIdClassMapping(id, true, true, packetClass);
+    }
+
+    public static Class<? extends Packet> getPacketClass(int id) {
+        return idToClassMap.get(id);
+    }
+
+    public static Integer getPacketId(Class<? extends Packet> packetClass) {
+        return classToIdMap.get(packetClass);
     }
 }
